a83d5ca71156e2e952aa6ed3d9b5668805eb81d2
Trevor Plassman <trevor@brasstacksweb.com>
2025-06-10 01:35:31 +0000

    Updating new condition controller action and template to support `allowAdminChanges` config setting.

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -5,3 +5,6 @@
 
 ## 2.0.0
 - Adding support for Craft CMS v5
+
+## 2.0.1 - 2025-06-09
+- Updating new condition controller action and template to support `allowAdminChanges` config setting.
--- src/controllers/ConditionsController.php
+++ src/controllers/ConditionsController.php
@@ -4,6 +4,7 @@ namespace brasstacksweb\craftbasicauth\controllers;
 
 use brasstacksweb\craftbasicauth\BasicAuth;
 use brasstacksweb\craftbasicauth\models\Condition;
+use craft\helpers\Cp;
 use craft\web\Controller;
 use yii\web\Response;
 
@@ -14,20 +15,46 @@ class ConditionsController extends Controller
 {
     public $defaultAction = 'index';
     protected array|bool|int $allowAnonymous = self::ALLOW_ANONYMOUS_NEVER;
+    private bool $readOnly = false;
+
+    public function init(): void
+    {
+        parent::init();
+
+        $this->readOnly = !\Craft::$app->getConfig()->getGeneral()->allowAdminChanges;
+    }
 
     public function actionIndex(): Response
     {
         $condition = \Craft::$app->getUrlManager()->getRouteParams()['condition']
             ?? new Condition();
+        $response = $this->asCpScreen()
+            ->title('New Authentication Condition')
+            ->action('craft-basic-auth/conditions/save')
+            ->redirectUrl('settings/plugins/craft-basic-auth')
+            ->addCrumb('All Conditions', 'settings/plugins/craft-basic-auth')
+            ->contentTemplate('craft-basic-auth/_condition.twig', [
+                'condition' => $condition,
+            ]);
 
-        return $this->renderTemplate('craft-basic-auth/_condition', [
-            'condition' => $condition,
-        ]);
+        if ($this->readOnly) {
+            $response
+                ->noticeHtml(Cp::readOnlyNoticeHtml())
+                ->action(null)
+                ->redirectUrl(null);
+        }
+
+        return $response;
     }
 
     public function actionSave(): ?Response
     {
         $this->requirePostRequest();
+
+        if ($this->readOnly) {
+            return $this->asFailure(\Craft::t('app', 'Admin changes not allowed.'));
+        }
+
         $condition = $this->request->getBodyParam('condition', []);
 
         $plugin = BasicAuth::getInstance();
